cbee3a466fdeaa887da778c27d795f7e4480f925
Nicolas Duvieusart Déry <nddery@gmail.com>
2019-04-12 17:54:02 -0400

    Start tmux with 256 color

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,7 +3,7 @@ alias rmds="find . -name '*.DS_Store' -type f -delete"
 
 alias ctags="`brew --prefix`/bin/ctags"
 alias mkdir='mkdir -p'
-alias t='tmux'
+alias tmux="TERM=screen-256color-bce tmux"
 
 alias la='ls -la'
 alias ll='ls -l'
