a91cad9740afd254356567b11c9c4af3fc366a37
Nicolas Duvieusart Déry <nddery@gmail.com>
2020-09-27 10:01:49 -0400

    Add open Xcode workspace alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -16,3 +16,5 @@ alias gb='git branch'
 alias gch='git checkout'
 alias gl='git log --pretty=format:"%C(yellow)%h %C(blue)%ad%C(red)%d %C(reset)%s%C(green) [%an]" --decorate --date=short'
 alias yesterday="git log --since '1 day ago' --oneline --author nddery@gmail.com"
+
+alias xcw="open -a "Xcode" *.xcworkspace"
