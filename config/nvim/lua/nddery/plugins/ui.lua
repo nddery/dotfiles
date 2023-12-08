d5782e915b90f842cb0daa013d77b1f908aad29d
Nicolas Duvieusart Déry <nddery@gmail.com>
2023-12-08 13:08:38 -0500

    chore: indentation

--- config/nvim/lua/nddery/plugins/ui.lua
+++ config/nvim/lua/nddery/plugins/ui.lua
@@ -6,15 +6,15 @@ return {
 		},
 		config = function()
 			vim.cmd([[
-        let NERDTreeShowBookmarks=1
-        let NERDTreeIgnore=['\.py[cd]$', '\~$', '\.swo$', '\.swp$', '^\.git$', '^\.hg$', '^\.svn$', '\.bzr$', '\.DS_Store$']
-        let NERDTreeChDirMode=0
-        let NERDTreeMouseMode=2
-        let NERDTreeShowHidden=1
-        let NERDTreeKeepTreeInNewTab=1
-        let NERDTreeWinSize=40
-        " let g:nerdtree_tabs_open_on_gui_startup=0
-      ]])
+	       let NERDTreeShowBookmarks=1
+	       let NERDTreeIgnore=['\.py[cd]$', '\~$', '\.swo$', '\.swp$', '^\.git$', '^\.hg$', '^\.svn$', '\.bzr$', '\.DS_Store$']
+	       let NERDTreeChDirMode=0
+	       let NERDTreeMouseMode=2
+	       let NERDTreeShowHidden=1
+	       let NERDTreeKeepTreeInNewTab=1
+	       let NERDTreeWinSize=40
+	       " let g:nerdtree_tabs_open_on_gui_startup=0
+	     ]])
 		end,
 	},
 
