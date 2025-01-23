8c58d8366a269b29330d893c46a9cb9cb7bffcdf
Nicolas <nddery@gmail.com>
2025-01-23 15:50:28 -0500

    fix: copilot

--- config/nvim/lua/nddery/plugins/copilot.lua
+++ config/nvim/lua/nddery/plugins/copilot.lua
@@ -1,22 +1,15 @@
 return {
 	"github/copilot.vim",
-	-- https://github.com/orgs/community/discussions/139368
-	tag = "v1.33.0",
 	init = function()
 		vim.g.copilot_no_tab_map = true
-	 end,
-  config = function()
-		-- why this one does not work but tab does ????
-		vim.api.nvim_set_keymap("i", "<S-CR>", 'copilot#Accept("\\<CR>")', { noremap = true, silent = true, expr = true })
+	end,
+	config = function()
+		vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+			expr = true,
+			replace_keycodes = false,
+		})
 
-    vim.api.nvim_set_keymap('i', '<C-[>', '<Plug>(copilot-previous)', {})
-    vim.api.nvim_set_keymap('i', '<C-]>', '<Plug>(copilot-next)', {})
-
-		vim.api.nvim_set_keymap(
-			"i",
-			"<S-Tab>",
-			'copilot#Accept("<CR>")',
-			{ noremap = true, silent = true, expr = true }
-		)
+		vim.api.nvim_set_keymap("i", "<C-[>", "<Plug>(copilot-previous)", {})
+		vim.api.nvim_set_keymap("i", "<C-]>", "<Plug>(copilot-next)", {})
 	end,
 }
